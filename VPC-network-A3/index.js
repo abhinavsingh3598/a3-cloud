1d9bfbdd47e99e01797b992fd9998acba033a991
Abhinav <ab935176@dal.ca>
2023-07-04 16:15:05 -0300

    change port

--- VPC-network-A3/index.js
+++ VPC-network-A3/index.js
@@ -53,6 +53,6 @@ app.get('/list-products', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000.');
+app.listen(4000, () => {
+  console.log('Server is running on port 4000.');
 });
